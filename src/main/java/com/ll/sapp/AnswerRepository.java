60657254e9bffbd3a9a4c0f844f9f4adecff2370
lshtngus727 <lshtngus727@naver.com>
2024-07-17 22:45:16 +0900

    work

---
+++ src/main/java/com/ll/sapp/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.ll.sapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
--- src/test/java/com/ll/sapp/SappApplicationTests.java
+++ src/test/java/com/ll/sapp/SappApplicationTests.java
@@ -1,13 +1,26 @@
 package com.ll.sapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class SappApplicationTests {
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @Test
     void contextLoads() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
     }
 
 }
