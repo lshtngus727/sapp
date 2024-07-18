826cc910c3e85bcb4fa7ace5cc82773650802454
lshtngus727 <lshtngus727@naver.com>
2024-07-18 11:22:29 +0900

    work

--- src/main/java/com/ll/sapp/question/QuestionService.java
+++ src/main/java/com/ll/sapp/question/QuestionService.java
@@ -1,8 +1,10 @@
 package com.ll.sapp.question;
 
+import com.ll.sapp.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.ll.sapp.DataNotFoundException;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +16,13 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
